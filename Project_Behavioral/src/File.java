4c89a192069c6a0ea2aeae82cddf09cd8490c80e
Antonio <tonehonk@gmail.com>
2024-04-08 17:56:26 +0300

    Remove unifinished project

--- Project_Behavioral/src/File.java
+++
@@ -1,7 +0,0 @@
-public class File extends AbstractFile {
-
-    @Override
-    public void addFile(AbstractFile file) {
-        throw new RuntimeException("Attempt to add file into regular file");
-    }
-}
--- Project_Behavioral/src/hash/ChecksumCalculator.java
+++
@@ -1,8 +0,0 @@
-package hash;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface ChecksumCalculator {
-    String calculate(InputStream is) throws IOException;
-}
